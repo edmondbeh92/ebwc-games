8187dc5b0dea3ab5dd8987b736d9d0e849d47ac0
edmondbeh92 <edmondbeh92@gmail.com>
2019-08-20 20:06:03 +0800

    wip

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
     <div>
+      <Header />
       <h1 className='btn btn-outline-primary rounded-pill px-5 m-5'>React test</h1>
     </div>
   );
