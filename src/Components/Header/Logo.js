207bb458ab7d37fb92d7e60da35afb3c4fd261ac
edmondbeh92 <edmondbeh92@gmail.com>
2019-08-23 18:12:34 +0800

    fix logo glow

--- src/Components/Header/Logo.js
+++ src/Components/Header/Logo.js
@@ -5,22 +5,14 @@ import '../../style/other.css';
 
 export const Logo = () => {
 
-    const addGlowClass = e => {
-        const icon = document.getElementById('home-button');
-        icon.classList.add('icon-glow');
-        console.log('on');
-    }
-
-    const removeGlowClass = e => {
-        const icon = document.getElementById('home-button');
-        icon.classList.remove('icon-glow');
-    }
+    const addGlowClass = e => e.target.querySelector('img').classList.add('icon-glow');
+    const removeGlowClass = e => e.target.querySelector('img').classList.remove('icon-glow');
 
     return (
         <div class='text-left col-9 col-sm-6 pl-4' onMouseEnter={addGlowClass} onMouseLeave={removeGlowClass}>
             <h3>
                 <a href="index.html" class='text-gray text-link'>
-                    <img src={require("../../images/ebwc_weblogo.png")} alt="EBWC_Logo" class='ebwc-icon' id='home-button' />
+                    <img src={require("../../images/ebwc_weblogo.png")} alt="EBWC_Logo" class='ebwc-icon' />
                     <span class='pl-1'>EBWC Games</span>
                 </a>
             </h3>
