814a05705c63302e03e2cf4d8e4ed06f51e5e6c2
edmondbeh92 <edmondbeh92@gmail.com>
2019-08-23 19:04:35 +0800

    fix

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Header } from './header/Header';
-import { Content } from './content/Content';
-import { Footer } from './footer/Footer';
+import { Header } from './Header/Header';
+import { Content } from './Content/Content';
+import { Footer } from './Footer/Footer';
 
 export const App = () => (
   <>
